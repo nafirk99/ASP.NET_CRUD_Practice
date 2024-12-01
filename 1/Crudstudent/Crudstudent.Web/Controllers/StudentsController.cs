4da4f22b11469bf807971e2316b7fcbc5d29d7c7
nafirk99 <nafiz.ba.pakizatvl@gmail.com>
2024-12-01 16:53:17 +0600

    Crud 1 Done

---
+++ 1/Crudstudent/Crudstudent.Web/Controllers/StudentsController.cs
@@ -0,0 +1,80 @@
+﻿using Crudstudent.Web.Data;
+using Crudstudent.Web.Models;
+using Crudstudent.Web.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crudstudent.Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel model)
+        {
+            var student = new Student
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Phone = model.Phone,    
+                Subscribed = model.Subscribed,
+            };
+
+            await _dbContext.AddAsync(student);
+            await _dbContext.SaveChangesAsync();
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List() 
+        {
+            var student = await _dbContext.Students.ToListAsync();
+            return View(student);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id) 
+        {
+            var student = await _dbContext.Students.FindAsync(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await _dbContext.Students.FindAsync(viewModel.Id);
+            if (student is not null)
+            {
+                student.Name = viewModel.Name;
+                student.Email = viewModel.Email;
+                student.Email = viewModel.Email;
+                student.Subscribed = viewModel.Subscribed;
+                await _dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel)
+        {
+            var student = await _dbContext.Students.FindAsync(viewModel.Id);
+            if (student is not null)
+            {
+                _dbContext.Students.Remove(student);
+                await _dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ 1/Crudstudent/Crudstudent.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Crudstudent.Web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crudstudent.Web.Data
+{
+    public class ApplicationDbContext: DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
